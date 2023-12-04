242dbc7701a5e32b46c0a98a3a30aa7f06db8830
Mohit Bidikar <mohitbidikar4@gmail.com>
2023-12-04 22:19:52 +0000

    project created

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# MERNboilerplate
+# ExpenseTracker
---
+++ frontend/src/api/axios.js
@@ -0,0 +1,24 @@
+import axiosDefault from "axios";
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const baseURL = isProduction
+  ? `http://localhost:5001` //Add production URL here
+  : "http://localhost:5001";
+
+const defaultOptions = {
+  baseURL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+
+const axios = axiosDefault.create(defaultOptions);
+
+axios.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  config.headers.Authorization = token || "";
+  return config;
+});
+
+export default axios;
